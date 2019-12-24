028a83ba782529538bbd0f6007e87fd1a1117996
Michael Lobanov <vloggesup@gmail.com>
2019-12-24 18:39:22 +0200

    add .env

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   languages: ['en'],
-  api: process.env.API  !== undefined ? process.env.API : 'https://explorer-api.testnet.noah-blockchain.com/api/v1/' ,
-  status: process.env.STATUS !== undefined ? process.env.STATUS : 'https://explorer-api.testnet.noah-blockchain.com/api/v1/status-page'
+  api: process.env.API !== undefined ? process.env.API : 'https://explorer-api.testnet.noah-blockchain.com/api/v1/',
+  status: process.env.API !== undefined ? process.env.API + 'status-page' : 'https://explorer-api.testnet.noah-blockchain.com/api/v1/status-page'
 }
 
 export default config
--- app/next.config.js
+++ app/next.config.js
@@ -1,8 +1,13 @@
 const withLess = require('@zeit/next-less')
 const withImages = require('next-images')
+require('dotenv').config()
 
 module.exports = withLess(
   withImages({
-    inlineImageLimit: 0
+    inlineImageLimit: 0,
+    env: {
+      API: process.env.API,
+      STATUS: process.env.STATUS
+    }
   })
 )
