5acc774f337620b17580800bc90be0f869317427
Michael Lobanov <vloggesup@gmail.com>
2019-12-28 02:38:50 +0200

    add default charts api

--- app/config/index.js
+++ app/config/index.js
@@ -2,6 +2,6 @@ const config = {
   languages: ['en'],
   api: process.env.API !== undefined ? process.env.API : 'https://explorer-api.testnet.noah-blockchain.com/api/v1/',
   status: process.env.API !== undefined ? process.env.API + 'status-page' : 'https://explorer-api.testnet.noah-blockchain.com/api/v1/status-page',
-  charts_api: process.env.API_CHARTS
+  charts_api: process.env.API_CHARTS !== undefined ? process.env.API_CHARTS : 'https://coin-price-backend.testnet.noah-blockchain.com/'
 }
 export default config
